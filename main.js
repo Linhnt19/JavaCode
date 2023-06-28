2d08f6a2ac7b5bec1bfe8206dd719d0cb30c6625
TuanLinh <ngotuanlinhvv@gmail.com>
2023-06-28 21:31:38 +0700

    add web file

---
+++ main.js
@@ -0,0 +1,50 @@
+/**
+ * 1.alert dua ra thong bao
+ * 2.console
+ * 3.confirm thing bao co huy
+ * 4.prompt thong bao co nhap
+ * 5.setTimeout
+ * 6.setInterval
+ * 7.kieu du lieu co ban
+ */
+// alert('day la thong bao')
+// console.log('Chu Ngan')
+// var a=9
+// var b=3
+// c=a+b
+// console.log(c)
+// var fullNames ='Ngo Tuan Linh'
+// //camelcase
+
+// console.log(fullNames)
+
+// // confirm("xac nhan du tuoi")
+
+// // prompt("ban bao nhieu tuoi")
+
+// setTimeout(function(){
+//     alert("Thong bao het hang")
+// },10)
+// setInterval(function(){
+// // alert("chao ban")
+// console.log("chao ban")
+// })
+// // dua ra thong bao qua mon
+// var diemTB=5;
+//  if (diemTB>=5) {
+//     console.log("ban da qua mon")
+//  } else {
+//     console.log("ban da truot")
+//  }
+
+
+
+var soMot = prompt("moi nhap a:")
+var soHai = prompt("moi nhap b:")
+
+var tong = Number(soMot) + Number(soHai)
+alert("Tong: "  + tong)
+alert(`Tong: ${tong}`)
+
+// S
+ 
